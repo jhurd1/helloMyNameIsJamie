6fa8609e3771d88f4a66647ea0cbd3598916c20e
Edgar Cobian <edgarcobian@live.com>
2021-01-27 20:54:34 -0800

    "I added Hello from edgar in Jamie"

--- src/main/java/helloMyNameIsJamie/helloMyNameIsJamie.java
+++ src/main/java/helloMyNameIsJamie/helloMyNameIsJamie.java
@@ -1,9 +1,11 @@
 package helloMyNameIsJamie;
 
-public class helloMyNameIsJamie
-{
-    public static void main (String[] args)
-    {
+public class helloMyNameIsJamie{
+    public static void main (String[] args){
         System.out.print("Hello, from Jamie.");
+        System.out.println("Hello, from Edgar");
+
     }
+
 }
+
