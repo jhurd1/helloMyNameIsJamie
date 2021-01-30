9fd6cdebdf7cf944c5249dc832cdcdcdb5bf93e3
cfowl <goose_7@msn.com>
2021-01-30 10:50:28 -0700

    said hello to Jamie from Chris

--- src/main/java/helloMyNameIsJamie/helloMyNameIsJamie.java
+++ src/main/java/helloMyNameIsJamie/helloMyNameIsJamie.java
@@ -4,7 +4,7 @@ public class helloMyNameIsJamie{
     public static void main (String[] args){
         System.out.print("Hello, from Jamie.");
         System.out.println("Hello, from Edgar");
-
+        System.out.println("Hello Jamie, my name is Chris.");
     }
 
 }
