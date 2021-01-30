23b88ec8950542a466f542f27056bf77c1b610f1
Ecob1 <78009340+Ecob1@users.noreply.github.com>
2021-01-30 11:09:15 -0800

    I just add more  text.

--- src/main/java/helloMyNameIsJamie/helloMyNameIsJamie.java
+++ src/main/java/helloMyNameIsJamie/helloMyNameIsJamie.java
@@ -4,6 +4,7 @@ public class helloMyNameIsJamie{
     public static void main (String[] args){
         System.out.print("Hello, from Jamie.");
         System.out.println("Hello, from Edgar");
+        System.out.print("Hello again, this is a test");
         System.out.println("Hello Jamie, my name is Chris.");
     }
 
